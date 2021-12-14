0da2ffcb2ead03d606ccf6b66212ffec3b7868f2
Volodymyr Krytskyi <vkrytskyy@gmail.com>
2021-12-14 15:25:26 +0200

    Mobile fix

--- src/index.tsx
+++ src/index.tsx
@@ -27,6 +27,12 @@ window.addEventListener('resize', () => {
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
+window.addEventListener('scroll', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 
 
 ReactDOM.render(
